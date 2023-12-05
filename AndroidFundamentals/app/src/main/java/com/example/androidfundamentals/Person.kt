7024d895c7ff54d6b4568351ae61227db0c2b03c
Gáll Botond <gallbotondofficial@gmail.com>
2023-12-05 22:29:19 +0200

    Revert "started the course"
    This reverts commit 040be9d0f3e4541bdc0d8aa1fce1f7e57769726f.

--- AndroidFundamentals/app/src/main/java/com/example/androidfundamentals/Person.kt
+++
@@ -1,8 +0,0 @@
-package com.example.androidfundamentals
-
-import java.io.Serializable
-
-data class Person(
-    val name: String,
-    val age: Int
-) : Serializable
--- AndroidFundamentals2/app/src/main/java/com/example/androidfundamentals2/Todo.kt
+++
@@ -1,6 +0,0 @@
-package com.example.androidfundamentals2
-
-data class Todo(
-    val title: String,
-    var isChecked: Boolean = false
-)
