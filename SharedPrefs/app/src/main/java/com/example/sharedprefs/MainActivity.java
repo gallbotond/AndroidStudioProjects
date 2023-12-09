e46984dec338cf7eacdf35400aebf46e4a5add8e
Gáll Botond <gallbotondofficial@gmail.com>
2023-12-09 13:34:19 +0200

    shared prefs

--- SharedPrefs/app/src/main/java/com/example/sharedprefs/MainActivity.java
+++ SharedPrefs/app/src/main/java/com/example/sharedprefs/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.sharedprefs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    EditText editText;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editTextText);
+        button = findViewById(R.id.button);
+
+        DisplaySavedText();
+
+        button.setOnClickListener(v -> {
+            String text = editText.getText().toString();
+            DisplayAndSaveText(text);
+        });
+    }
+
+    private void DisplaySavedText() {
+        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE);
+        String text = sharedPreferences.getString("text", "");
+        textView.setText(text);
+    }
+
+    private void DisplayAndSaveText(String text) {
+        textView.setText(text);
+        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("text", text);
+        editor.commit();
     }
 }
